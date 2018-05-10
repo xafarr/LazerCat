4c7bef12ff85e0d55e6fc096799bd250a52f8979
Muhammad Zafar <xafarr@gmail.com>
2018-05-10 11:20:22 +1000

    fixed some print statements

--- lazercat/stepper2.py
+++ lazercat/stepper2.py
@@ -48,13 +48,13 @@ StepCounter = 0
 # Start main loop
 while True:
 
-    print("StepCounter = {StepCounter}")
-    print("Seq[StepCounter] = {Seq[StepCounter]}")
+    print("StepCounter = ${StepCounter}")
+    print("Seq[StepCounter] = ${Seq[StepCounter]}")
 
     for pin in range(0, 4):
         xpin = StepPins[pin]
         if Seq[StepCounter][pin] != 0:
-            print(" Enable GPIO {xpin}")
+            print(" Enable GPIO ${xpin}")
             GPIO.output(xpin, True)
         else:
             GPIO.output(xpin, False)
