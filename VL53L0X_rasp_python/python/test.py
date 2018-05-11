8e3da5ff4c956872be0eb45d4cdfb1cd00f47875
Muhammad Zafar <xafarr@gmail.com>
2018-05-11 10:14:57 +1000

    Added test script

--- VL53L0X_rasp_python/python/test.py
+++ VL53L0X_rasp_python/python/test.py
@@ -1,55 +1,51 @@
-#!/usr/bin/python
-
-# MIT License
-# 
-# Copyright (c) 2017 John Bryan Moore
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import sys
 import time
+from multiprocessing import Process
 
 import RPi.GPIO as GPIO
 import VL53L0X
 
-# Create a VL53L0X object
-tof = VL53L0X.VL53L0X()
+rocket = 0
 
-# Start ranging
-tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+def rangeFinder():
+    # Create a VL53L0X object
+    tof = VL53L0X.VL53L0X()
+    
+    # Start ranging
+    tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+    
+    timing = tof.get_timing()
+    if (timing < 20000):
+        timing = 20000
+    print("Timing %d ms" % (timing / 1000))
+    
+    count = 0
+    while True:
+        count = +count
+        distance = tof.get_distance()
+        if (distance > 0):
+            print("%d mm, %d cm, %d" % (distance, (distance / 10), count))
+    
+        time.sleep(timing / 1000000.00)
+    
+    
+    # tof.stop_ranging()
 
-timing = tof.get_timing()
-if (timing < 20000):
-    timing = 20000
-print("Timing %d ms" % (timing / 1000))
 
-count = 0
-while True:
-    count = +count
-    distance = tof.get_distance()
-    if (distance > 0):
-        print("%d mm, %d cm, %d" % (distance, (distance / 10), count))
-
-    time.sleep(timing / 1000000.00)
+def func1():
+    global rocket
+    print('start func1')
+    while rocket < 100000:
+        rocket += 1
+    print('end func1')
 
 
-# tof.stop_ranging()
+def func2():
+    global rocket
+    print('start func2')
+    while rocket < 10000:
+        rocket += 1
+    print('end func2')
 
 def setupStepperPins():
     # Use BCM GPIO references
@@ -136,9 +132,10 @@ def mainStepper():
         # Wait before moving on
         time.sleep(WaitTime)
 
-# if __name__ == '__main__':
-#     setupStepperPins()
-#     p1 = Process(target=mainStepper)
-#     p2 = Process(target=func2)
-#     p1.start()
-#     p2.start()
+
+if __name__ == '__main__':
+    setupStepperPins()
+    p1 = Process(target=mainStepper)
+    p2 = Process(target=func2)
+    p1.start()
+    p2.start()
