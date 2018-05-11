8246699664a5833dd706fd291be55407e5c743b0
Muhammad Zafar <xafarr@gmail.com>
2018-05-11 12:31:35 +1000

    Added led in separate thread

--- VL53L0X_rasp_python/python/catRangeFinder.py
+++ VL53L0X_rasp_python/python/catRangeFinder.py
@@ -22,16 +22,16 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import os
+import random
 import sys
 import time
-from gtts import gTTS
-import os
 from multiprocessing import Process
+from test import mainStepper
+
 import RPi.GPIO as GPIO
 import VL53L0X
-import random
 
-from test import setupStepperPins, mainStepper
 
 def setupStepperPins():
     # Use BCM GPIO references
@@ -118,20 +118,110 @@ def mainStepper():
         # Wait before moving on
         time.sleep(WaitTime)
 
+
 def washYourHands():
     washChoices = ['this is the police, wash your hands',
                    'wash your hands you filthy animal',
                    'please wash your hands']
     washChoice = random.choice(washChoices)
     os.system("espeak \"" + washChoice + "\" 2>/dev/null ")
-    
+
+# Define a simple function to turn on the LED colors
+def color(R, G, B, on_time):
+    # Color brightness range is 0-100%
+    RED.ChangeDutyCycle(R)
+    GREEN.ChangeDutyCycle(G)
+    BLUE.ChangeDutyCycle(B)
+
+    REDD.ChangeDutyCycle(R)
+    GREENN.ChangeDutyCycle(G)
+    BLUEE.ChangeDutyCycle(B)
+    time.sleep(on_time)
+
+    # Turn all LEDs off after on_time seconds
+    RED.ChangeDutyCycle(0)
+    GREEN.ChangeDutyCycle(0)
+    BLUE.ChangeDutyCycle(0)
+
+    REDD.ChangeDutyCycle(0)
+    GREENN.ChangeDutyCycle(0)
+    BLUEE.ChangeDutyCycle(0)
+
+def lightLoop():
+    # Main loop
+    try:
+        # while RUNNING:
+        for x in range(0, 1000):
+            for x in range(0, 2):
+                for y in range(0, 2):
+                    for z in range(0, 2):
+                        print(x, y, z)
+                        # Slowly ramp up power percentage of each active color
+                        for i in range(0, 101):
+                            color((x * i), (y * i), (z * i), .002)
+
+    # If CTRL+C is pressed the main loop is broken
+    # except KeyboardInterrupt:
+    #     RUNNING = False
+    #     print("Quitting")
+
+    # Actions under 'finally' will always be called
+    # regardless of what stopped the program
+    finally:
+        # Stop and cleanup so the pins
+        # are available to be used again
+        GPIO.cleanup()
+
+
 def runWarning():
     stepper = Process(target=mainStepper)
     voice = Process(target=washYourHands)
+    light = Process(target=lightLoop)
     stepper.start()
     voice.start()
+    light.start()
+
 
 if __name__ == '__main__':
+    # Set GPIO to Broadcom system and set RGB Pin numbers
+    RUNNING = True
+    GPIO.setmode(GPIO.BCM)
+    red = 4
+    green = 5
+    blue = 6
+    
+    redd = 13
+    greenn = 19
+    bluee = 26
+    
+    
+    # Set pins to output mode
+    GPIO.setup(red, GPIO.OUT)
+    GPIO.setup(green, GPIO.OUT)
+    GPIO.setup(blue, GPIO.OUT)
+    
+    GPIO.setup(redd, GPIO.OUT)
+    GPIO.setup(greenn, GPIO.OUT)
+    GPIO.setup(bluee, GPIO.OUT)
+    
+    
+    Freq = 100 #Hz
+    
+    # Setup all the LED colors with an initial
+    # duty cycle of 0 which is off
+    RED = GPIO.PWM(red, Freq)
+    RED.start(0)
+    GREEN = GPIO.PWM(green, Freq)
+    GREEN.start(0)
+    BLUE = GPIO.PWM(blue, Freq)
+    BLUE.start(0)
+    
+    REDD = GPIO.PWM(redd, Freq)
+    REDD.start(0)
+    GREENN = GPIO.PWM(greenn, Freq)
+    GREENN.start(0)
+    BLUEE = GPIO.PWM(bluee, Freq)
+    BLUEE.start(0)
     # setupStepperPins()
     # p1 = Process(target=mainStepper)
     # p2 = Process(target=func2)
@@ -140,17 +230,17 @@ if __name__ == '__main__':
     # Create a VL53L0X object
     tof = VL53L0X.VL53L0X()
     setupStepperPins()
-    #tts=gTTS(text='Wash you hands, animal', lang='en')
-    #tts.save("wash.mp3")
-    
+    # tts=gTTS(text='Wash you hands, animal', lang='en')
+    # tts.save("wash.mp3")
+
     # Start ranging
     tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
-    
+
     timing = tof.get_timing()
     if (timing < 20000):
         timing = 20000
     print("Timing %d ms" % (timing / 1000))
-    
+
     count = 0
     while True:
         count = +count
@@ -161,8 +251,7 @@ if __name__ == '__main__':
                 tof.stop_ranging()
                 runWarning()
                 tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
-    
+
         time.sleep(timing / 1000000.00)
-    
-    
+
     # tof.stop_ranging()
